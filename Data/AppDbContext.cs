2145fffb60d5ef4ecb5327b828e45ddf717d94e7
Jo├úo Paulo <joaopaulo102003@gmail.com>
2025-07-01 09:53:53 -0300

    Tarefa_Completa

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using GerenciadorTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorTarefas.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Tarefa> Tarefas { get; set; }
+}
