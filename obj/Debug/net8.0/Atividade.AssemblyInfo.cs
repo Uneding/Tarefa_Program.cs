172db7d201715566adc293aabb35ad65ff7f88de
Jo├úo Paulo <joaopaulo102003@gmail.com>
2025-07-08 09:49:47 -0300

    C:/Users/Aluno/AppData/Local/Programs/Git/root

--- obj/Debug/net8.0/Atividade.AssemblyInfo.cs
+++ obj/Debug/net8.0/Atividade.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8e5c66f3e3711496955acd397b96a1bfec0cbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
